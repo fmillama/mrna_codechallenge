a02696faaaca9a1b8f1534069d0298dd620e5206
Milla Martinez <francesc.milla-martinez-external@capgemini.com>
2021-03-04 12:01:30 +0100

    edit

--- test_genchal.py
+++ test_genchal.py
@@ -21,21 +21,21 @@ class TestGenChal(unittest.TestCase):
         mock1=genunf([],'') #passes empty arguments
         self.assertEqual(mock1,['',''])
         mock2=genunf(['UCC'],'') #unfinished gene warning
-        self.assertEqual(mock2,["Gene: ['UCC']. ",''])
+        self.assertEqual(mock2,['', "Gene: ['UCC']. "])
         mock3=genunf([],'U') #unfinished codon warning
-        self.assertEqual(mock3,['',"Codon: U."])
+        self.assertEqual(mock3,['Codon: U.', ''])
         mock4=genunf(['UCC'],'U')
-        self.assertEqual(mock4,["Gene: ['UCC']. ","Codon: U."])
+        self.assertEqual(mock4,['Codon: U.', "Gene: ['UCC']. "])
 
     def test_genproc(self):
         mock1=[gen for gen in genproc(io.StringIO('ACg ACG UAAACG uAA UAACCCUAA'))] #executes without errors and ignores consecutive stop codon
         self.assertEqual(mock1,[['ACG', 'ACG', 'UAA'],['ACG', 'UAA'],['CCC', 'UAA']])
         mock2=[gen for gen in genproc(io.StringIO('ACG ACG UAA ACG'))] #yields one gene and invalid ending error
-        self.assertEqual(mock2,[['ACG', 'ACG', 'UAA'], "Error: end of stream reached with unfinished elements. Gene: ['ACG']. \nSequence must have length divisible by 3 and end in stop codon UAG, UGA or UAA."])
+        self.assertEqual(mock2,[['ACG', 'ACG', 'UAA'], "Error: sequence must end in stop codon UAG, UGA or UAA. Process complete with unfinished elements. Gene: ['ACG']. "])
         mock3=[gen for gen in genproc(io.StringIO('ACG ACG UAA AC'))] #yields one gene and invalid length error
-        self.assertEqual(mock3,[['ACG', 'ACG', 'UAA'], 'Error: end of stream reached with unfinished elements. Codon: AC.\nSequence must have length divisible by 3 and end in stop codon UAG, UGA or UAA.'])
+        self.assertEqual(mock3,[['ACG', 'ACG', 'UAA'], 'Error: sequence must have length divisible by 3. Process complete with unfinished elements. Codon: AC.'])
         mock4=[gen for gen in genproc(io.StringIO('ACG UAA ECG'))] #yields one gene and invalid letter error
-        self.assertEqual(mock4,[['ACG', 'UAA'], 'Error: will ignore E found at line 1, column 7. Letters must be A, C, G or U.', 'Error: end of stream reached with unfinished elements. Codon: CG.\nSequence must have length divisible by 3 and end in stop codon UAG, UGA or UAA.'])
+        self.assertEqual(mock4,[['ACG', 'UAA'], 'Error: will ignore E found at line 1, column 7. Letters must be A, C, G or U.', 'Error: sequence must have length divisible by 3. Process complete with unfinished elements. Codon: CG.'])
 
 if __name__=='__main__':
     unittest.main()
